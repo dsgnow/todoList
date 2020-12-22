caa3a034ca58d8c5cce8b84d009db8c9311a31e8
Piotr Stachowicz <ptr.stachowicz@gmail.com>
2020-12-22 19:02:46 +0100

    add drag and drop

--- js/main.js
+++ js/main.js
@@ -19,6 +19,7 @@ const addTask = (e) => {
         const spanDelete = document.createElement('span');
         const imgDelete = document.createElement('img');
         li.classList.add('todoList__elem', 'todoList__elem--active');
+        li.setAttribute("draggable", "true");
         spanCircle.classList.add('circle', 'todoList__circle');
         imgCircle.setAttribute('src', 'images/check.svg');
         imgCircle.classList.add('circle__img');
@@ -54,7 +55,6 @@ const removeTask = (e) => {
 }
 
 const completeTask = (e) => {
-  console.log(e.target);
   e.target.closest('span').classList.toggle("todoList__circle--completed");
   e.target.closest('li').classList.toggle("todoList__elem--completed");
   e.target.firstElementChild.classList.toggle("circle__img--completed");
@@ -70,7 +70,6 @@ const searchCompleted = (e) => {
   document.querySelector('.filter__completed').classList.toggle("filter--active");
   let tasks = [...liElements];
   tasks = tasks.filter(li => li.classList.contains("todoList__elem--completed"));
-  console.log(tasks);
   ul.textContent = '';
   tasks.forEach(li => ul.appendChild(li));
   itemsLeftcount = tasks.length;
@@ -124,3 +123,69 @@ document.querySelector('.filter__active').addEventListener('click', searchActive
 document.querySelector('.main__changeColor').addEventListener('click', changeColor);
 
 
+//drag and drop
+document.addEventListener("drag", function( event ) {
+
+}, false);
+
+document.addEventListener("dragstart", function( event ) {
+    // store a ref. on the dragged elem
+    dragged = event.target;
+    // make it half transparent
+    event.target.style.opacity = .9;
+}, false);
+
+document.addEventListener("dragend", function( event ) {
+    // reset the transparency
+    event.target.style.opacity = "";
+}, false);
+
+/* events fired on the drop targets */
+document.addEventListener("dragover", function( event ) {
+  // prevent default to allow drop
+  event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", function( event ) {
+  // highlight potential drop target when the draggable element enters it
+  if (event.target.classList.contains("todoList__elem") ) {
+      event.target.style.background = "hsl(0, 0%, 98%)";
+  }
+
+}, false);
+
+document.addEventListener("dragleave", function( event ) {
+  // reset background of potential drop target when the draggable element leaves it
+  if ( event.target.classList.contains("todoList__elem") ) {
+      event.target.style.background = "";
+  }
+
+}, false);
+
+document.addEventListener("drop", function( event ) {
+  // prevent default action (open as link for some elements)
+  let indexEvent = '';
+  let indexDragged = '';
+
+  const actualLiElems = [...document.querySelectorAll('li p')];
+  
+  actualLiElems.forEach(function (actualLiElem, index) {
+    if (actualLiElem.textContent == event.target.textContent) {
+      indexEvent = index;
+    }
+    if (actualLiElem.textContent == dragged.textContent) {
+      indexDragged = index;
+    }
+});
+
+  console.log(indexDragged, indexEvent);
+  event.preventDefault();
+  // move dragged elem to the selected drop target
+  if ( event.target.classList.contains("todoList__elem") ) {
+      event.target.style.background = "";
+      // dragged.parentNode.removeChild( dragged );
+      indexDragged>indexEvent?event.target.insertAdjacentElement('beforebegin', dragged):event.target.insertAdjacentElement('afterend', dragged);
+      liElements = document.querySelectorAll('li');
+  }
+
+}, false);
